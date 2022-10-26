7e5d860dbc97a476e76621be14c8263be113661e
Aislan Santos - Sedundario <aislan.santos@gmail.com>
2022-10-26 17:22:16 -0300

    ajuste de visualização de estados

--- ibge/urls.py
+++ ibge/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name='index'),
-    path('estados/', views.estados, name='estados'),
+    path('<id:int>/estados/', views.estados, name='estados'),
 ]
